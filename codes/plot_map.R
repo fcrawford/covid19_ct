6172c2c164920ef805970e26334727f75a416b3a
Richard Li <lizehang@gmail.com>
2020-04-14 12:11:22 -0400

    add adj matrix and plot codes

---
+++ codes/plot_map.R
@@ -0,0 +1,17 @@
+library(rgdal)
+library(SUMMER)
+
+# Load shapefile
+CTmap <- readOGR("../map/wgs84/countyct_37800_0000_2010_s100_census_1_shp_wgs84.shp")
+
+# create and save adj matrix
+adj <- getAmat(CTmap, CTmap$NAME10)
+write.csv(adj, file = "../map/CT_adj_matrix.csv", quote=FALSE, row.names=TRUE)
+
+# Quick plot
+plot(CTmap)
+
+# Plot with names
+mapPlot(data=NULL, geo=CTmap, by.geo="NAME10")
+
+
