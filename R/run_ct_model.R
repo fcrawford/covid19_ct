1790a2a89ccc5b4da52f3a77ca869ec95afed845
Richard Li <lizehang@gmail.com>
2020-04-19 15:17:39 -0400

    plots now take data and additional param as input

--- R/run_ct_model.R
+++ R/run_ct_model.R
@@ -170,13 +170,29 @@ sir_results_summary <- sir_results_long %>% group_by(variable, time) %>%
 
 ####################
 # plotting 
+# @param data column include [variable, time, mean, lower, upper]
 # @param which.plot the prefix of the compartment to plot, e.g., S, E, I_s, I_m. If a vector of more than one specified, it plots multiple lines
-# @param add logical, if adding capacity line                         
+# @param title the tile of the figure
+# @param xlab the xlab of the figure
+# @param ylab the ylab of the figure
+# @param color the color of the line
 
-plot_ct_region = function(region_name, which.plot = "D", add=FALSE) {
-  #compartment_plot_labels = c("D")
-  #compartment_plot_names = c("Deaths")
-  #compartment_plot_colors = rainbow(length(compartment_plot_labels))
+
+plot_ct_region = function(data = sir_results_summary, 
+                          region_name = "Connecticut", 
+                          which.plot = "D", 
+                          color =  NULL,
+                          title=NULL, xlab=NULL, ylab=NULL,
+                          tmax.plot = tmax,
+                          start_day = day0,
+                          end_day = daymax,
+                          lab_where = daymonthseq, 
+                          lab_show = monthseq_lab,
+                          capacity_func = county_capacities,
+                          obs_state = dat_ct_state,
+                          obs_county = dat_ct_county) {
+
+
   lab.table <- data.frame(compartment=c("D","rD",
                                         "H","rH",
                                         "Hbar", "rHbar", "rHsum",
@@ -194,39 +210,43 @@ plot_ct_region = function(region_name, which.plot = "D", add=FALSE) {
                                     "Asymptomatic Infections"))
 
   which.plot.ci <- which.plot
+  add <- FALSE
   if("rH" %in% which.plot) add <- TRUE
   if("rHsum" %in% which.plot) add <- TRUE
 
   par(mar=c(3,4,3,0), bty="n")
   toplot <- paste(rep(which.plot,each=length(region_name)),
                   rep(region_name, length(which.plot)),sep=".")
-  sir_result_region= filter(sir_results_summary, variable%in%toplot)
+  sir_result_region= filter(data, variable%in%toplot)
 
-  title <- paste0(lab.table$labels[lab.table$compartment==which.plot[1]], " in ", region_name)
-  ymax <- max(sir_result_region$mean[sir_result_region$time <= tmax], na.rm=TRUE)
+  if(is.null(title)){
+    title <- paste0(lab.table$labels[lab.table$compartment==which.plot[1]], " in ", region_name)
+  }
+  ymax <- max(sir_result_region$mean[sir_result_region$time <= tmax.plot], na.rm=TRUE)
   
   if(add){
-    if(region_name %in% names(county_capacities)){
-        sub.add <- data.frame(time = 0:tmax, 
-                              Capacity = county_capacities[[region_name]](0:tmax))
+    if(region_name %in% names(capacity_func)){
+        sub.add <- data.frame(time = 0:tmax.plot, 
+                              Capacity = capacity_func[[region_name]](0:tmax.plot))
     }else if(region_name == "Connecticut"){
-      cap.state <- rep(0, tmax+1)
-      for(nm in 1:length(county_capacities)){
-          cap.state <- cap.state + county_capacities[[nm]](0:tmax)
+      cap.state <- rep(0, tmax.plot+1)
+      for(nm in 1:length(capacity_func)){
+          cap.state <- cap.state + capacity_func[[nm]](0:tmax.plot)
       }
-      sub.add <- data.frame(time = 0:tmax, Capacity = cap.state)
+      sub.add <- data.frame(time = 0:tmax.plot, Capacity = cap.state)
     }else{
       stop(paste0(region_name, " not recognized in capacity function"))
     }
     ymax <- max(ymax, sub.add$Capacity)
   }
-
-  plot(0, type="n", xlab="", ylab="People", main=title, col="black", 
-       ylim=c(0,1.05*ymax), xlim=c(0,1.1*tmax), axes=FALSE)
-  axis(1,at=daymonthseq, lab=monthseq_lab)
+  if(is.null(xlab)) xlab <- ""
+  if(is.null(ylab)) ylab <- "People"
+  plot(0, type="n", xlab=xlab, ylab=ylab, main=title, col="black", 
+       ylim=c(0,1.05*ymax), xlim=c(0,1.1*tmax.plot), axes=FALSE)
+  axis(1,at=lab_where, lab=lab_show)
   axis(2)
 
-  abline(v=Sys.Date()-day0, col="gray", lty=2)
+  abline(v=Sys.Date()-start_day, col="gray", lty=2)
 
 
   lab.table$color <- as.character(lab.table$color)
@@ -238,12 +258,12 @@ plot_ct_region = function(region_name, which.plot = "D", add=FALSE) {
       if(which.plot[i] %in% c("rH", "rHbar", "rHsum")){
         time.print <- which.max(sir_result_region_sub$mean)
       }else{
-        time.print <- tmax + 1
+        time.print <- tmax.plot + 1
       }
       polygon(c(sir_result_region_sub$time, rev(sir_result_region_sub$time)), c(sir_result_region_sub$lower, rev(sir_result_region_sub$upper)), col=col.polygon, border=NA)
-      text(sir_result_region_sub$time[tmax+1], sir_result_region_sub$mean[time.print], format(sir_result_region_sub$mean[time.print],digits=2, big.mark=","), pos=4, col=col.line)
-      text(sir_result_region_sub$time[tmax+1], sir_result_region_sub$lower[time.print], format(sir_result_region_sub$lower[time.print],digits=2, big.mark=","), pos=4, col=col.polygon)
-      text(sir_result_region_sub$time[tmax+1], sir_result_region_sub$upper[time.print], format(sir_result_region_sub$upper[time.print],digits=2, big.mark=","), pos=4, col=col.polygon)
+      text(sir_result_region_sub$time[tmax.plot+1], sir_result_region_sub$mean[time.print], format(sir_result_region_sub$mean[time.print],digits=2, big.mark=","), pos=4, col=col.line)
+      text(sir_result_region_sub$time[tmax.plot+1], sir_result_region_sub$lower[time.print], format(sir_result_region_sub$lower[time.print],digits=2, big.mark=","), pos=4, col=col.polygon)
+      text(sir_result_region_sub$time[tmax.plot+1], sir_result_region_sub$upper[time.print], format(sir_result_region_sub$upper[time.print],digits=2, big.mark=","), pos=4, col=col.polygon)
     }
     lines(sir_result_region_sub$time, sir_result_region_sub$mean, col=col.line)
   }
@@ -255,11 +275,11 @@ plot_ct_region = function(region_name, which.plot = "D", add=FALSE) {
   # Add observed deaths
   col.line <- lab.table$color[which(lab.table$compartment=="D")]
   if(region_name == "Connecticut" && "rD" %in% which.plot) {
-    points(dat_ct_state$time, dat_ct_state$deaths, pch=16, cex=0.6, col=col.line) 
+    points(obs_state$time, obs_state$deaths, pch=16, cex=0.6, col=col.line) 
   } else if("rD" %in% which.plot) {
-    obs.region <- subset(dat_ct_county, county == region_name)
+    obs.region <- subset(obs_county, county == region_name)
     obs.region$date <- ymd(obs.region$date)
-    first.region.time <- round(as.numeric(difftime(obs.region$date[1], day0, units="days")),0)
+    first.region.time <- round(as.numeric(difftime(obs.region$date[1], start_day, units="days")),0)
     obs.region$time <- c(first.region.time:(nrow(obs.region)+first.region.time-1)) # add time variable that indexes time 
     #obs.region <- merge(obs.region, date.time, by='date')
     points(obs.region$time, obs.region$deaths, pch=16, cex=0.6, col=col.line)
@@ -268,28 +288,24 @@ plot_ct_region = function(region_name, which.plot = "D", add=FALSE) {
   # Add observed hospitalization
   col.line <- lab.table$color[which(lab.table$compartment=="H")]
   if(region_name == "Connecticut" && ("rH" %in% which.plot || "rHsum" %in% which.plot))  {
-      points(dat_ct_state$time, dat_ct_state$cur_hosp, pch=16, cex=0.6, col=col.line) 
+      points(obs_state$time, obs_state$cur_hosp, pch=16, cex=0.6, col=col.line) 
   } else if("rH" %in% which.plot || "rHsum" %in% which.plot) {
-    obs.region <- subset(dat_ct_county, county == region_name)
+    obs.region <- subset(obs_county, county == region_name)
     obs.region$date <- ymd(obs.region$date)
-    first.region.time <- round(as.numeric(difftime(obs.region$date[1], day0, units="days")),0)
+    first.region.time <- round(as.numeric(difftime(obs.region$date[1], start_day, units="days")),0)
     obs.region$time <- c(first.region.time:(nrow(obs.region)+first.region.time-1)) # add time variable that indexes time 
     #obs.region <- merge(obs.region, date.time, by='date')
     points(obs.region$time, obs.region$cur_hosp, pch=16, cex=0.6, col=col.line)
   }
 
-  #legend(0, max(dat_ct_state$deaths), compartment_plot_names, lty=1, col=compartment_plot_colors, bty="n")
 
-  # return some useful info
-  # capacity exceeded?
-  # describe intvx
   region_summary <- NULL
   if("D" %in% which.plot || "rD" %in% which.plot){
       sir_result_region_sub <- filter(sir_result_region, variable==paste0(which.plot[1],".",region_name))
-      count <- sir_result_region_sub$mean[sir_result_region_sub$time==tmax]
-      count.min <- sir_result_region_sub$lower[sir_result_region_sub$time==tmax]
-      count.max <- sir_result_region_sub$upper[sir_result_region_sub$time==tmax]
-      region_summary = paste(region_summary, "On ", format(daymax, "%B %d"),
+      count <- sir_result_region_sub$mean[sir_result_region_sub$time==tmax.plot]
+      count.min <- sir_result_region_sub$lower[sir_result_region_sub$time==tmax.plot]
+      count.max <- sir_result_region_sub$upper[sir_result_region_sub$time==tmax.plot]
+      region_summary = paste(region_summary, "On ", format(end_day, "%B %d"),
                        " projections show ", format(count, digits=2, big.mark=","),
                        " cumulative deaths reported in ", region_name,
                        " with 90% uncertainty interval between ", format(count.min, digits=2, big.mark=","),
@@ -303,7 +319,7 @@ plot_ct_region = function(region_name, which.plot = "D", add=FALSE) {
       peak <- which.max(sir_result_region_sub$mean)
       count.min <- sir_result_region_sub$lower[peak]
       count.max <- sir_result_region_sub$upper[peak]
-      region_summary = paste(region_summary, "On ", format(daymax, "%B %d"),
+      region_summary = paste(region_summary, "On ", format(end_day, "%B %d"),
                        " projections show a peak of ", format(count, digits=2, big.mark=","),
                        " hospitalizations reported in ", region_name,
                        " with 90% uncertainty interval between ", format(count.min, digits=2, big.mark=","), 
@@ -317,7 +333,7 @@ plot_ct_region = function(region_name, which.plot = "D", add=FALSE) {
       peak <- which.max(sir_result_region_sub$mean)
       count.min <- sir_result_region_sub$lower[peak]
       count.max <- sir_result_region_sub$upper[peak]
-      region_summary = paste(region_summary, "On ", format(daymax, "%B %d"),
+      region_summary = paste(region_summary, "On ", format(end_day, "%B %d"),
                        " projections show a peak of ", format(count, digits=2, big.mark=","),
                        " required hospitalizations reported in ", region_name,
                        " with 90% uncertainty interval between ", format(count.min, digits=2, big.mark=","), 
@@ -328,17 +344,16 @@ plot_ct_region = function(region_name, which.plot = "D", add=FALSE) {
 
   return(region_summary)
 }
-
 #####################################
 #
 # @param which.plot the prefix of the compartment to plot, e.g., S, E, I_s, I_m. If a vector of more than one specified, it takes the sum of the compartment (only the mean!)
-mapplot_ct_region = function(which.plot = "D", label = "Cumulative Deaths", palette="Reds", ...) {
+mapplot_ct_region = function(data=sir_results_summary, which.plot = "D", label = "Cumulative Deaths", palette="Reds", ...) {
   map = CTmap
   #ncol=4 # customize based on date range? 
   region_names <- as.character(map$NAME10)
   toplot <- paste(rep(which.plot,each=length(region_names)),
                   rep(region_names, length(which.plot)),sep=".")
-  sir_result_internal = data.frame(filter(sir_results_summary, variable%in%toplot))
+  sir_result_internal = data.frame(filter(data, variable%in%toplot))
   sir_result_internal$County <- sir_result_internal$variable
   for(i in which.plot) sir_result_internal$County <- gsub(paste0(i,"\\."), "", sir_result_internal$County)
   
@@ -420,7 +435,13 @@ get_Cumulative <- function(date, tosum){
 #sapply(region_names, plot_ct_region)
 
 # test plot multiple lines
-plot_ct_region("Connecticut", c("rH"))
+
+# plot_ct_region(data = sir_results_summary, 
+#                region_name = "Connecticut", 
+#                which.plot = "rD", 
+#                title=NULL, xlab=NULL, ylab=NULL)
+
+
 
 # for(i in region_names){
 #   summary <- plot_ct_region(i, c("H"), add = dat_ct_capacity)
