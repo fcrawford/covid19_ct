fb74fd0002835312ceb5c5f4919eb5e688d9ee48
Olya Morozova <olga.morozova@yale.edu>
2020-05-26 17:04:19 -0400

    state-wide hospitalization update: 05/21 - 05/26; fix distancing release to be 10%, posterior distancing  = posterior lockdown

--- functions/plot_report_scenario.R
+++ functions/plot_report_scenario.R
@@ -22,29 +22,29 @@ mydistancing_on_date  = mylockdown_end_date+1 # distancing on at end of lockdown
 
 if(scenario_name == "fast") {
   str = "\nwith release on 5/20 and biweekly increments in contact"
-  mydistancing_stepdown_dates = seq(mydistancing_on_date+1, mydistancing_on_date+1+days(14*10), by=14)
+  mydistancing_stepdown_dates = seq(mydistancing_on_date+1, mydistancing_on_date+1+days(14*9), by=14)
 } else {
   str = "\nwith release on 5/20 and monthly increments in contact"
-  mydistancing_stepdown_dates = seq(mydistancing_on_date+1, mydistancing_on_date+1+days(30*10), by=30)
+  mydistancing_stepdown_dates = seq(mydistancing_on_date+1, mydistancing_on_date+1+days(30*9), by=30)
 }
 
 
 ## choose your scenario and get state0, params and a sample from posterior
 
-# base scenario (low asymptomatic): q_A = 0.36, mean(q_Is) = 0.064
+# scenario 1: low asymptomatic: q_A = 0.36, mean(q_Is) = 0.064
 if(case_name == "low"){
     mystate0 = get_state0("../data/ct_init.csv")
     myparams = yaml.load_file("../parameters/params.yml")  
     myposterior = read.csv("../data/posterior_a036.csv", stringsAsFactors=FALSE)     
 }
 
-# alternative scenario 1 (medium asymptomatic): q_A = 0.5, mean(q_Is) = 0.05
+# scenario 2: medium asymptomatic: q_A = 0.5, mean(q_Is) = 0.05
 if(case_name == "medium"){
     mystate0 = get_state0("../data/ct_init_a05.csv")
     myparams = yaml.load_file("../parameters/params_a05.yml")  
     myposterior = read.csv("../data/posterior_a05.csv", stringsAsFactors=FALSE) 
 }
-# alternative scenario 2 (high asymptomatic): q_A = 0.7, mean(q_Is) = 0.03
+# scenario 3: high asymptomatic: q_A = 0.7, mean(q_Is) = 0.03
 if(case_name == "high"){
     mystate0 = get_state0("../data/ct_init_a07.csv")
     myparams = yaml.load_file("../parameters/params_a07.yml")  
@@ -62,8 +62,8 @@ myparams$distancing_effect = myparams$lockdown_effect
 #myposterior$testing_effect_A = rtruncdist(nrow(myposterior), mean=0.2, sd=0.03, lower=0.1, upper=0.3)
 #myposterior$testing_effect_Im = rtruncdist(nrow(myposterior), mean=0.5, sd=0.04, lower=0.35, upper=0.65)
 
-myposterior$distancing_effect = myparams$distancing_effect
-
+#myposterior$distancing_effect = myparams$distancing_effect
+myposterior$distancing_effect = myposterior$lockdown_effect
 
 ####################################
 if(file.exists(paste("../output/", scenario_name, "_", case_name, ".RData", sep=""))){
